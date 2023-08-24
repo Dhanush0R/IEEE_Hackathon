f565b1e9ca7b778945e5e34a4db6d3ee12424d02
Bhuvan C S <205bhuvics@gmail.com>
2023-08-24 17:03:04 +0530

    backend asset and admin api endpoints

--- backend/controller/asset.controller.js
+++ backend/controller/asset.controller.js
@@ -5,17 +5,25 @@ import Asset from '../models/asset.model.js';
 // @access Public
 const getAssets = async (req, res) => {
     const queries = req.query.query;
+    const state = req.query.state;
+    const district = req.query.district;
+    console.log(district);
+    const taluk = req.query.taluk;
     console.log(queries);
     try {
-        const queryArray = queries.split(',').map(query => query.trim());
+        const queryArray = queries && queries.split(',').map(query => query.trim());
     
         const assets = await Asset.find({
           $or: [
             { name: { $in: queryArray, $regex: '.*', $options: 'i' } },
-            { district: { $in: queryArray, $regex: '.*', $options: 'i' } },
-            { taluk: { $in: queryArray, $regex: '.*',$options: 'i' } },
-            { state: { $in: queryArray, $regex: '.*', $options: 'i' } }
-          ],
+            {
+            $and: [
+                { district: {  $regex: district, $options: 'i' } },
+                { taluk: {  $regex: taluk,$options: 'i' } },
+                { state: {  $regex: state, $options: 'i' } }
+            ],
+            }
+          ]
         });
     
         res.status(200).json(assets);
@@ -25,4 +33,91 @@ const getAssets = async (req, res) => {
       }
 };
 
-export {getAssets}
+
+// @desc Get Asset by ID
+// route GET /api/assets/:assetId
+// @access Public
+const getAssetById = async (req, res) => {
+    const assetId = req.params.assetId;
+    try {
+    
+        const asset = await Asset.findById(assetId);
+    
+        res.status(200).json(asset);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching assets.' });
+      }
+};
+
+// @desc Add an Asset
+// route POST /api/assets
+// @access Admin
+const addAsset = async (req, res) => {
+    const newAsset = new Asset({
+        type: req.body.type,
+        name: req.body.name,
+        location: req.body.location,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        size: req.body.size,
+        yearOfConstruction: req.body.yearOfConstruction,
+        capacity: req.body.capacity,
+        rooms: req.body.rooms,
+        labs: req.body.labs,
+        maintananceRequired: req.body.maintananceRequired,
+        presentUse: req.body.presentUse,
+        actionHistory: [],
+        state: req.body.state,
+        district: req.body.district,
+        taluk: req.body.taluk
+    })
+    newAsset.save()
+    .then(() => {return res.json("New asset added!")})
+    .catch((err) => {return res.status(400).json('Error: '+ err)})
+};
+
+// @desc Update an Asset
+// route POST /api/assets
+// @access Admin
+const updateAsset = async (req, res) => {
+    const newAsset = new Asset({
+        type: req.body.type,
+        name: req.body.name,
+        location: req.body.location,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        size: req.body.size,
+        yearOfConstruction: req.body.yearOfConstruction,
+        capacity: req.body.capacity,
+        rooms: req.body.rooms,
+        labs: req.body.labs,
+        maintananceRequired: req.body.maintananceRequired,
+        presentUse: req.body.presentUse,
+        actionHistory: [],
+        state: req.body.state,
+        district: req.body.district,
+        taluk: req.body.taluk
+    })
+    newAsset.save()
+    .then(() => {return res.json("New asset added!")})
+    .catch((err) => {return res.status(400).json('Error: '+ err)})
+};
+
+// @desc Delete Asset by ID
+// route DELETE /api/assets/:assetId
+// @access Public
+const delAsset = async (req, res) => {
+    const assetId = req.params.assetId;
+    try {
+    
+        const asset = await Asset.findByIdAndDelete(assetId);
+    
+        res.status(200).json({message: 'Asset deleted successfully'});
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching assets.' });
+      }
+};
+
+export {getAssets, getAssetById, addAsset, delAsset}
--- backend/routes/asset.routes.js
+++ backend/routes/asset.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getAssets} from '../controller/asset.controller.js';
+import {getAssets, getAssetById, addAsset, delAsset} from '../controller/asset.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAssets);
-
+router.get('/:assetId', getAssetById);
+router.post('/', addAsset)
+router.delete('/:assetId', delAsset);
 
 export default router;
