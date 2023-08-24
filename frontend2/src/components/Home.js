1e3bf8b0a43d5cd47ee8e90d2245e2929ba28a00
Chinmay <chinmaygs406@gmail.com>
2023-08-25 01:02:44 +0530

    frontend 5

--- frontend2/src/components/Home.js
+++ frontend2/src/components/Home.js
@@ -1,13 +1,13 @@
 // Home.js
 import React from 'react';
-import CarouselComponent from './Carousel'; // Import the Carousel component
+// import CarouselComponent from './Carousel'; // Import the Carousel component
 import './Home.css'; // Import the Home.css file
 
 const Home = () => {
   return (
     <div className="home-container">
       <h2>Welcome to Shalaa Mitra</h2>
-      <CarouselComponent /> {/* Use the Carousel component */}
+      {/* <CarouselComponent /> Use the Carousel component */}
       {/* <img src="public/1.jpeg" alt="Header" className="header-image" /> */}
       {/* Other content */}
     </div>
