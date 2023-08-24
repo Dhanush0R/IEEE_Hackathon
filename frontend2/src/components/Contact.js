33e9c65c4f4a9b5763cae666d53b5c94b8153fdf
Chinmay <chinmaygs406@gmail.com>
2023-08-24 15:18:34 +0530

    frontend

---
+++ frontend2/src/components/Contact.js
@@ -0,0 +1,19 @@
+// Contact.js
+import React from 'react';
+import './Contact.css'; // Import the Contact.css file
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <h1 className="contact-heading">Contact Us</h1>
+      <form className="contact-form">
+        <input type="text" className="contact-input" placeholder="Name" />
+        <input type="email" className="contact-input" placeholder="Email" />
+        <textarea className="contact-textarea" placeholder="Message"></textarea>
+        <button className="contact-button">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
